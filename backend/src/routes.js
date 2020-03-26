0e8534238aaae9506c30aeb2b6597090ae5a5298
williamwjd <william.jose.wjd@gmail.com>
2020-03-26 08:27:36 -0300

    deletando um incident

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,5 +9,6 @@ routes.post('/ongs', OngController.store)
 
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.store)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes
