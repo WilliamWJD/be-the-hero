7c3893206769f6233198602ad895fb9de562a1c0
williamwjd <william.jose.wjd@gmail.com>
2020-03-26 15:52:55 -0300

    configurando o cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 const port = process.env.PORT_SERVER || 3333
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
