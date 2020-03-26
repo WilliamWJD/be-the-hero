379f3e2bddfb5cd629e33e8c8a85de159be23367
williamwjd <william.jose.wjd@gmail.com>
2020-03-26 15:46:28 -0300

    criando join de incidents com ongs

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,23 +1,31 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async index(req, res){
+    async index(req, res) {
         const { page = 1 } = req.query
 
-        const [ count ] = await connection('incidents').count()
+        const [count] = await connection('incidents').count()
 
         res.header('X-Total-Count', count['count(*)'])
 
         const incidents = await connection('incidents')
-        .limit(5)
-        .offset((page - 1)*5)
-        .select('*')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*', 
+                'ongs.name', 
+                'ongs.email', 
+                'ongs.whatsapp', 
+                'ongs.city', 
+                'ongs.uf'
+            ])
         return res.json(incidents)
     },
 
-    async store(req, res){
+    async store(req, res) {
         const { title, description, value } = req.body
-        const  ong_id  = req.headers.authorization
+        const ong_id = req.headers.authorization
 
         const [id] = await connection('incidents').insert({
             title,
@@ -29,16 +37,16 @@ module.exports = {
         return res.json({ id })
     },
 
-    async delete(req,res){
+    async delete(req, res) {
         const { id } = req.params
-        const  ong_id  = req.headers.authorization
+        const ong_id = req.headers.authorization
 
         const incident = await connection('incidents')
-        .where('id', id)
-        .select('ong_id')
-        .first()
+            .where('id', id)
+            .select('ong_id')
+            .first()
 
-        if(incident.ong_id !== ong_id){
+        if (incident.ong_id !== ong_id) {
             return res.status(401).json({ error: 'Operation not permitted' })
         }
 
