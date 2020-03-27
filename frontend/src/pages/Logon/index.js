fb280273591ca613a0bcf5f5f03ae5a4c636f0c8
williamwjd <william.jose.wjd@gmail.com>
2020-03-27 08:10:54 -0300

    iniciando construção da página de logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import {FiLogIn} from 'react-icons/fi'
 import './styles.css'
+
+import heroesImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function logon(){
     return(
-        <h1>Hellow Word</h1>
+       <div className="logon-container">
+           <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+
+                <form action="">
+                    <h1>Faça seu logon</h1>
+                    <input type="text" placeholder="Sua ID"/>
+                    <button type="submit">Entrar</button>
+                    <a href="#">
+                        <FiLogIn size={16} color="#e02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+           </section>
+
+           <img src={heroesImg} alt="Heroes"/>
+       </div> 
     )
 }
