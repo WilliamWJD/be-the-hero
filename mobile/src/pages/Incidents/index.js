30ff079f04b9eda1730bdf6763d6f510c5efbc2f
williamwjd <william.jose.wjd@gmail.com>
2020-03-28 16:51:43 -0300

    criando a página de incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,10 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native'
 
-export default function Incidents(){
-    return(
-        <View>
-            <Text>Incidents</Text>
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles'
+
+export default function Incidents() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos</Text>
+                </Text>
+            </View>
+            <Text style={styles.title}>Bem-vindo</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+            <FlatList
+                style={styles.incidentList}
+                data={[1, 2, 3]}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$: 120,000</Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => { }}>
+                            <Text style={styles.detailButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     )
 }
