cf5d797eccfc3e2dc2f30d3445a6386aa6026f9b
williamwjd <william.jose.wjd@gmail.com>
2020-03-28 09:34:52 -0300

    Cadastrando um novo incident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
+
 import './styles.css'
+
+import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident(){
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
+
+    const ongId = localStorage.getItem('ongId')
+
+    const history = useHistory()
+
+    async function handleNewIncident(e){
+        e.preventDefault()
+        const data = {
+            title, description, value
+        }
+        try {
+            await api.post('/incidents', data ,{
+                headers: {
+                    authorization: ongId
+                }
+            })
+            history.push('/profile')
+        } catch (error) {
+            alert('Erro ao cadastrar caso, tente novamente.')
+        }
+    }
+
     return(
         <div className="new-incident-container">
             <div className="content">
@@ -19,10 +46,25 @@ export default function NewIncident(){
                         Voltar para profile
                     </Link>
                 </section>
-                <form action="">
-                    <input type="text" placeholder="Título do caso"/>
-                    <textarea type="email" placeholder="Descrição"/>
-                    <input placeholder="Valor em reais"/>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        type="text" 
+                        placeholder="Título do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        type="text" 
+                        placeholder="Descrição"
+                        placeholder="Descrição"
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Valor em reais"
+                        placeholder="Valor"
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import api from '../../services/api'
@@ -13,6 +13,8 @@ export default function Profile() {
 
     const ongName = localStorage.getItem('ongName')
     const ongId = localStorage.getItem('ongId')
+
+    const history = useHistory()
 
     useEffect(() => {
         api.get('/profile', {
@@ -37,13 +39,18 @@ export default function Profile() {
         }
     }
 
+    function handleLogout(){
+        localStorage.clear()
+        history.push('/')
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
