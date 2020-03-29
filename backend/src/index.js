e16cbf9cd6851f8a5c7a72cc5a122101823258d5
williamwjd <william.jose.wjd@gmail.com>
2020-03-29 16:50:25 -0300

    criando validação com o celebrate no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
+const { errors } = require('celebrate')
+
 require('dotenv').config()
 
 const app = express()
@@ -10,6 +12,7 @@ const port = process.env.PORT_SERVER || 3333
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 app.listen(port, ()=>{
     console.log(`Servidor online`)
